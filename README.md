f5a0b03549898182c511ec355d4ed9c26d0150a0
sambein <samuel.bein@gmail.com>
2017-06-20 15:12:33 -0500

    added genfrags and built README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # StealthSusy
+
+git clone https://github.com/sbein/StealthSusy.git
+
+cmsrel CMSSW_8_0_5_patch1
+cd CMSSW_8_0_5_patch1/src
+cmsenv
+git-cms-addpkg Configuration/Generator
+cp -r ../../StealthSusy/genfrags/\* Configuration/Generator/python
+cp -r ../../StealthSusy/SLHA .
+cmsenv
+scram b 
+cd ../../
+
+cmsDriver.py Stealth_test_cff --conditions auto:run2_mc --fast --era Run2_2016 --eventcontent AODSIM --relval 100000,1000 -s GEN,SIM,RECOBEFMIX,DIGI:pdigi_valid,L1,DIGI2RAW,L1Reco,RECO,EI,HLT:@relval25ns --datatier AODSIM --beamspot Realistic50ns13TeVCollision --fileout Stealth_test_AODSIM_n0.root --no_exec -n 100
+
+cmsRun cmsRun Stealth_test_cff_GEN_SIM_RECOBEFMIX_DIGI_L1_DIGI2RAW_L1Reco_RECO_EI_HLT.py
---
+++ genfrags/Stealth_test_cff.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    #crossSection = cms.untracked.double(5.72e+07),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    comEnergy = cms.double(13000.0),
+    SLHAFileForPythia8  = cms.string('SLHA/HadronicStealthAll.slha'),    
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'SUSY:gg2squarkantisquark = on',
+            'SUSY:qqbar2squarkantisquark = on',
+            'print:quiet=false'
+            'SLHA:allowUserOverride = true',  
+            'SLHA:keepSM = on',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
