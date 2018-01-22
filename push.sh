247f25d9dc936fc6edd472ecd50087477ce386ca
sambein <samuel.bein@gmail.com>
2018-01-22 09:09:01 -0600

    update to configs

--- push.sh
+++ push.sh
@@ -1,1 +1,2 @@
 git remote set-url origin https://sbein@github.com/sbein/StealthSusy.git && git push origin master
+#potentially first line: git remote set-url origin ssh://git@github.com/sbein/StealthSusy.git
